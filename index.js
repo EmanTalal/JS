3346725ddb6672aca75a90868f36e062ae3f9254
github_EmanTalal <emantalal.201999@gmail.com>
2024-05-21 20:56:54 +0300

    LUB 2

--- index.js
+++ index.js
@@ -45,3 +45,77 @@ var adjective3 = "entertaining";
 var madLib = "The Intro to JavaScript course is" + adjective1 + "James and Julia are so"
 adjective2 + "cannot wait to work through the rest of this " + adjective3 + "content!"
 console.log("The Total is :" + madLib)
+
+
+
+//Lub 2
+
+//Musical Groups
+let musicians = 2;
+if (musicians <= 0) {
+    console.log("not a group");
+} else if (musicians == 1) {
+    console.log("solo");
+} else if (musicians == 2) {
+    console.log("duet");
+} else if (musicians == 1) {
+    console.log("trio");
+} else if (musicians == 4) {
+    console.log("quartet");
+} else("Non!");
+
+//Murder mystery
+let room = " ballroom";
+let suspect = " Mr. Parkes";
+let weapon = "";
+let solv = false;
+if (room === "ballroom " && suspect === "Mr. Parkes") {
+    if (room === "ballroom") {
+        weapon = "poison";
+    } else if (room === "gallery") {
+        weapon = "trophy";
+    } else if (room === "billiards") {
+        weapon = "pool stick";
+    } else if (room === "dining room") {
+        weapon = "knife";
+    }
+}
+
+if (solv) {
+    console.log(suspect + " did it in the " + room + " with the " + weapon);
+}
+
+//Checking Your Balance
+var balance = -325;
+var checkBalance = true;
+var isActive = true;
+
+if (checkBalance === true) {
+    if (balance == -325) {
+        if (checkBalance == true) {
+            if (isActive)
+                console.log("You pass all REQUIREMENTS!")
+        }
+    }
+} else("Sorry!Not Allow you");
+
+//Ice Cream
+let flavor = "chocolate";
+let vessel = "bowl";
+let toppings = "sprinkles";
+if (flavor == "chocolate" && flavor == "vanilla" && vessel == "bowl" && vessel == "cone" && toppings == "sprinkles" && toppings == peanuts) {
+    console.log("I'd like two scoops of " + flavor + " ice cream in a" + vessel + " with" + sprinkles);
+} else {
+    console.log("Sorry!Don't Have ..Back Soon!")
+}
+
+
+//WH..What do I Wear? 
+let shirtWidt = 32;
+let shirtLength = 30;
+let shirtSleeve = 8.71;
+if (shirtWidt == 18 || shirtWidt == 28 || shirtWidt == 8 || shirtWidt == 13) {
+    concaats.log("is S")
+} else if (shirtLength == 20 || shirtLength == 29 || shirtLength == 8 || shirtLength == 38) {
+    concaats.log("This is M!")
+}
