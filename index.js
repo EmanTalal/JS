4763f260ebc6f346cc4f7ff39b8339995ab26319
github_EmanTalal <emantalal.201999@gmail.com>
2024-05-21 18:39:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//Culcate The Rectangle!
+rectangle = 30;
+Sequer = 50;
+result = rectangle * Sequer;
+console.log("The area of the rectangle is " + result)
+
+
+//trnslate degres
+temperature = 50;
+F = temperature * 1.8 + 32;
+console.log("The temperature in F is:" + F)
+Desgres = 9 / 5 * F + 32;
+console.log("The temperature in Degre is:" + Desgres)
+
+
+//Translate Hour to munit
+Hour = 5;
+munit = Hour * 3600;
+console.log("The munit is:" + munit)
+
+
+//Concatenation
+thingOne = "Eman";
+thingTwo = "Talal";
+let concaats = thingOne.concat("", thingTwo)
+console.log(concaats)
+
+//Total bill
+var bill = 10.25 + 3.99 + 7.15;
+var tip = 0.15;
+var total = bill + tip;
+console.log("The Total is :" + total)
+
+// Awesome Message
+firstName;
+interest;
+hobby;
+let awesomeMessage;
+
+//Mad Libs
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var madLib = "The Intro to JavaScript course is " + adjective1.
+"James and Julia are so $(adjective2).I cannot wait to work through the rest of this + $(adjective3) content!'
+console.log("The Total is :" + total) console.log("The Total is :" + total)
+"
+console.log("The Total is :" + madLib)
